340fccb19d7e94ca3256f7371a41ce5978c67910
ryan armbrust <ryan.armbrust@gmail.com>
2011-12-18 01:19:16 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ffengineering::Application.config.secret_token = 'ed8b11f8fb6c24fd54034a0771d630e0356df360715af9904cfc25fdb0f33b7977e4d8c6c1a452b825134722dd6a6b12fef8d5241a745106c5ec5b242c7a471c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ffengineering::Application.config.session_store :cookie_store, :key => '_ffengineering_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ffengineering::Application.config.session_store :active_record_store
